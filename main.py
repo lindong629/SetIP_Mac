a651aef076b475b995372b73a0cfd35c76bc87bc
林东 <lindong@MacBook-Pro13.local>
2020-08-08 14:56:33 +0800

    修改主文件运行判断

--- main.py
+++ main.py
@@ -3,29 +3,36 @@
 # Jimmy Lin
 import tools_change_ip
 
-while True:
 
-    # 显示菜单
-    tools_change_ip.show_menu()
+def main():
+    
+    while True:
 
-    action = input("请选择操作功能： ")
+        # 显示菜单
+        tools_change_ip.show_menu()
 
-    # 根据用户选择决定后续操作。 1、查看网卡信息，0、退出系统
-    if action == "1":
-        tools_change_ip.show_interface_list()
-        tools_change_ip.action()
+        action = input("请选择操作功能： ")
 
-    elif action == "2":
-        tools_change_ip.show_interface_list()
-        tools_change_ip.action_me()
+        # 根据用户选择决定后续操作。 1、查看网卡信息，0、退出系统
+        if action == "1":
+            tools_change_ip.show_interface_list()
+            tools_change_ip.action()
 
-    elif action == "3":
-        tools_change_ip.show_interface_list()
-        tools_change_ip.action_dhcp()
+        elif action == "2":
+            tools_change_ip.show_interface_list()
+            tools_change_ip.action_me()
 
-    elif action == "0":
-        print("欢迎再次使用【IP管理系统】")
-        break
+        elif action == "3":
+            tools_change_ip.show_interface_list()
+            tools_change_ip.action_dhcp()
 
-    else:
-        print("输入错误，请重新选择")
+        elif action == "0":
+            print("欢迎再次使用【IP管理系统】")
+            break
+
+        else:
+            print("输入错误，请重新选择")
+
+
+if __name__ == "__main__":
+    main()
